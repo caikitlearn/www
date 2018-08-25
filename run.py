83f7a3228578a34c3daa6c08ee68ba14a70894b2
Ubuntu <ubuntu@ip-172-31-34-191.us-east-2.compute.internal>
2018-08-25 16:16:29 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from www import app
+app.run(host='0.0.0.0',debug=True)
+# app.run(debug=True)
---
+++ www/views.py
@@ -0,0 +1,58 @@
+from www import app # init file
+
+from flask import render_template, jsonify, request
+import pandas as pd
+import numpy as np
+
+##==================================================##
+##   HOME PAGE                                      ##
+##==================================================##
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+##==================================================##
+##   DATA SCIENCE                                   ##
+##==================================================##
+@app.route('/datascience')
+def datascience():
+    return render_template('datascience/datascience.html')
+
+@app.route('/hot-hub-time-machine')
+def hot_hub_time_machine():
+    return render_template('datascience/hot-hub-time-machine.html')
+
+@app.route('/the-making-of-hot-hub-time-machine')
+def the_making_of_hot_hub_time_machine():
+    return render_template('datascience/the-making-of-hot-hub-time-machine.html')
+
+@app.route('/scraping-data-from-basketball-reference')
+def scraping_data_from_basketball_reference():
+    return render_template('datascience/scraping-data-from-basketball-reference.html')
+
+@app.route('/basketball-reference-hall-of-fame-probabilities')
+def basketball_reference_hall_of_fame_probabilities():
+    return render_template('datascience/basketball-reference-hall-of-fame-probabilities.html')
+
+##==================================================##
+##   STATISTICS                                     ##
+##==================================================##
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics/statistics.html')
+
+##==================================================##
+##   MISC                                           ##
+##==================================================##
+@app.route('/misc')
+def misc():
+    return render_template('misc/misc.html')
+
+@app.route('/tractor')
+def tractor():
+    return render_template('misc/tractor.html')
+
+##==================================================##
+##   HOT HUB TIME MACHINE BGP                       ##
+##==================================================##
